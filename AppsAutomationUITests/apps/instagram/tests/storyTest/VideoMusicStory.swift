e78e53fc51fe16cfd9d0125c8313f3fd90a34ced
Geetansh2 <geetanshbhatia12@gmail.com>
2020-07-07 01:22:58 +0530

    trying to get the meminfo

--- AppsAutomationUITests/apps/instagram/tests/storyTest/VideoMusicStory.swift
+++ AppsAutomationUITests/apps/instagram/tests/storyTest/VideoMusicStory.swift
@@ -30,7 +30,11 @@ class VideoMusicStory: XCTestCase{
     }
     
     func testAddVideoWithMusicFirstTime(){
+      
         commonUtils.launchApp(app: insta.getBundler())
+        sleep(5)
+          let start = report_memory()
+            sleep(5)
         insta.homescreenIG.clickStoryButton()
         print(instaApp.frame.height)
         print(instaApp.frame.width)
@@ -39,7 +43,7 @@ class VideoMusicStory: XCTestCase{
         instaApp.collectionViews.cells["Music sticker"].tap()
 //        instaApp.textFields["Search Music"].typeText("Hey")
         instaApp.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0)).withOffset(CGVector(dx: x/2, dy: y/9)).tap()
-       
+
         instaApp.typeText("Tu hi yaar mera")
         sleep(5)
         instaApp.staticTexts["Tu Hi Yaar Mera"].tap()
@@ -48,5 +52,28 @@ class VideoMusicStory: XCTestCase{
         sleep(2)
         instaApp.collectionViews.cells.otherElements["Button to share the message to Your Story"].tap()
         instaApp.buttons["Done"].tap()
+        let end = report_memory()
+        print ("-----------------------------------------------------------")
+        print ("Memory at finish: \(Int(Int(end) - Int(start)) / 1024 )")
+        sleep(10)
+        
+    }
+    
+    func report_memory()-> UInt64 {
+    var taskInfo = mach_task_basic_info()
+    var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
+    let kerr: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
+        $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+            task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
+        }
+    }
+
+        if kerr == KERN_SUCCESS {
+            print("Memory in use (in bytes): \(taskInfo.resident_size)")
+        }
+        else {
+            print("Error with task_info(): ")
+        }
+        return taskInfo.resident_size
     }
 }
